6e7bf092b5ae825ff9e433130cf7b628b3d85eec
DownKAX <rmskdk67@gmail.com>
2025-08-19 17:18:58 +0300

    aioredis

--- app/auth/redis_repository.py
+++ app/auth/redis_repository.py
@@ -5,15 +5,15 @@ from app.my_redis_client import get_redis
 
 async def add_session(user: str, refresh_token: str, session: Session):
     r = await get_redis()
-    r.hset(user, refresh_token, session.model_dump_json())
-    r.hset('refresh_tokens', refresh_token, user)
+    await r.hset(user, refresh_token, session.model_dump_json())
+    await r.hset('refresh_tokens', refresh_token, user)
 
 async def get_session(refresh_token: str) -> Session:
     """Returns the session to compare UserAgent, stored in redis, with current UserAgent to get new refresh token"""
     r = await get_redis()
     username = r.hget('refresh_tokens', refresh_token)
     if username:
-        session_in_redis = r.hget(username, refresh_token)
+        session_in_redis = await r.hget(username, refresh_token)
         if session_in_redis:
             session_in_redis = Session(**json.loads(session_in_redis))
         return session_in_redis
