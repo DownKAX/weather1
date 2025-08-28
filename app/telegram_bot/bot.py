7dd143d4d4c8758c5eb44cf1a6c592f129b3af90
DownKAX <rmskdk67@gmail.com>
2025-08-28 17:00:37 +0300

    New

--- app/telegram_bot/bot.py
+++ app/telegram_bot/bot.py
@@ -3,19 +3,18 @@ from aiogram_dependency import setup_dependency
 from aiogram import Bot, Dispatcher
 
 from app.core.settings import settings
-from app.telegram_bot.handlers.routers import router
-from app.telegram_bot.handlers.newsletter import news
+from app.telegram_bot.handlers.handler_registration import register_handlers
 
 bot = Bot(settings.TELEGRAM_API)
 
-async def send_newsletter_message(user_id, message):
-    await bot.send_message(user_id, message)
+async def send_newsletter_message(user_id, message, tg_bot=bot):
+    await tg_bot.send_message(user_id, message)
     await asyncio.sleep(0.3)
 
 async def main():
+
     dp = Dispatcher()
-    dp.include_router(router)
-    dp.include_router(news)
+    register_handlers(dp)
 
     setup_dependency(dp)
 
--- app/telegram_bot/handlers/routers.py
+++ app/telegram_bot/handlers/routers.py
@@ -1,50 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram import F, Router, types
+from aiogram import types
 from aiogram.fsm.state import StatesGroup, State
-from datetime import UTC, datetime
-
-from app.utils.forecast_api import forecast
+from httpx import AsyncClient
 from app.telegram_bot.handlers.dependecies import user_dependency, cities_dependency
 from app.telegram_bot.keyboards.common_keyboards import weather_markup
 
 class NewCityState(StatesGroup):
     new_city = State()
 
-router = Router()
-
-@router.message(Command("weather"))
 async def weather(message: types.Message):
     await message.answer(text='Узнать прогноз погоды:', reply_markup=weather_markup)
 
-@router.message(F.text=='Прогноз на сегодня')
-@router.message(F.text=='Прогноз на завтра')
-@router.message(F.text=='Прогноз на сегодня(кратко)')
-@router.message(F.text=='Прогноз на завтра(кратко)')
 async def today(message: types.Message,
-                user_service: user_dependency, city_service: cities_dependency, short_flag = False):
+                user_service: user_dependency):
     city_id = await user_service.select_user({'telegram_id': message.from_user.id}, return_value='city_id')
-    city_data = await city_service.select_city({'id': city_id})
-    day = message.text
-    if message.text.find('(кратко)') != -1:
-        short_flag = True
-        day = message.text.replace('(кратко)', '')
+    body = {'city_id': city_id, 'forecast_range': message.text}
+    if message.text.find('кратко') != -1:
+        body.update({'short_flag': True})
+        body['forecast_range'] = message.text.replace('(кратко)', '')
+    async with AsyncClient() as client:
+        forecast = await client.post('http://localhost:80/user/get_forecast', data=body)
+        forecast = forecast.json().get('forecast')
+    await message.answer(text=forecast)
 
-    forecast_data = await forecast.get_forecast(latitude=city_data.latitude,
-                                          longitude=city_data.longitude,
-                                          forecast_range=day,
-                                          current_hour=datetime.now(UTC).hour,
-                                          analysis_mark=short_flag)
-    await message.answer(text=forecast_data)
-
-@router.message(F.text=="Изменить город")
 async def change_city(message: types.Message, state: FSMContext):
     await message.answer(text='Введите следующим сообщением название вашего города')
     await state.set_state(NewCityState.new_city)
 
-@router.message(NewCityState.new_city)
 async def change_new_city(message: types.Message, state: FSMContext,
                           city_service: cities_dependency, user_service: user_dependency):
     city_id = await city_service.select_city({'city_name': message.text}, return_value='id')
@@ -55,3 +39,6 @@ async def change_new_city(message: types.Message, state: FSMContext,
         await message.answer(text=f'Город успешно изменён на: {message.text}')
         await state.clear()
         await state.set_state(None)
+
+async def registration_required(message: types.Message):
+    await message.answer(text='Вы не зарегистрированы в нашей системе, пройдите регистрацию')
